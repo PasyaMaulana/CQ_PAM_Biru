60fad6e676ba26edcb86cd0266281ca9930765bf
PasyaMaulana <pasyamaulana17@gmail.com>
2026-03-12 10:51:38 +0700

    menambahkan method untuk inisialisasi koneksi ke database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -16,6 +16,13 @@ namespace PraktikumADO
         SqlConnection conn;
         SqlCommand cmd;
 
+        private void Koneksi()
+        {
+            conn = new SqlConnection(
+                "Data Source=PASYA\\PASYA;Initial Catalog=DBAkademikADO;Integrated Security=True"
+            );
+        }
+
         public Form1()
         {
             InitializeComponent();
