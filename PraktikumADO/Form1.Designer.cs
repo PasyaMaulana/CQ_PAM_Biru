1c4f681360d5a31081616aa428a1d959ac970a34
PasyaMaulana <pasyamaulana17@gmail.com>
2026-03-12 10:29:12 +0700

    menampilkan struktur methon label1_Click

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -82,6 +82,7 @@
             this.label1.Size = new System.Drawing.Size(75, 31);
             this.label1.TabIndex = 4;
             this.label1.Text = "Hasil";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtHasil
             // 
--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -22,5 +22,10 @@ namespace PraktikumADO
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
